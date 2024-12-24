d95e5c2662a0a1fbf48e790d9836fc247ac49a27
Farhan899 <farhanbhai2u@gmail.com>
2024-12-24 19:28:09 +0500

    t1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 
-
-import Navbar from "./Navbar";
-import Hero_sec from "./Hero_sec";
+import Hero from "@/components/Herro";
+import FontShowcase from "@/components/Retangle";
+import NewArivel from "../components/arrivals";
+import BestSelling from "../components/Selling";
+import Dress from "@/components/dress";
+import CustomerCarousel from "@/components/Customer";
+import Products from "./product/page";
+import Top_sell from "./product/sell";
 
 
 export default function Home() {
   return (
-    <main>
-<Navbar/>
-<div></div>
-<Hero_sec />
-    </main>
+   <div>
+   
+    <Hero/>
+    <FontShowcase/>
+    {/* <NewArivel/>
+    <BestSelling/> */}
+    <Products/>
+    <Top_sell/>
+    <Dress/>
+    <CustomerCarousel/>
+    
+   </div> 
   );
 }
